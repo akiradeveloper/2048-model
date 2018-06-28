fa4f36427e70239036cf831d19b81c0efd03b078
Akira Hayakawa <ruby.wktk@gmail.com>
2018-06-28 17:57:26 +0900

    fix tests
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/Model0Spec.scala
+++ src/test/scala/Model0Spec.scala
@@ -1,80 +1,80 @@
 import model_2048.Model0
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, Matchers}
 
-class Model0Spec extends FlatSpec {
+class Model0Spec extends FlatSpec with Matchers {
   import Model0._
   it should "swipe 2" in {
-    val result = swipe0(Array(Some(2),None,None))
-    assert(result.state === Array(Some(2),None,None))
-    assert(result.moves === Array(Move(0,0,false)))
-    assert(result.doubles === Array())
+  val result = swipe0(Seq(Some(2),None,None))
+    result.state shouldBe Seq(Some(2),None,None)
+    result.moves shouldBe Seq(Move(0,0,false))
+    result.doubles shouldBe Seq.empty
   }
   it should "swipe 2,4" in {
-    val result = swipe0(Array(Some(2),Some(4)))
-    assert(result.state === Array(Some(2),Some(4)))
-    assert(result.moves === Array(Move(0,0,false),Move(1,1,false)))
-    assert(result.doubles === Array())
+    val result = swipe0(Seq(Some(2),Some(4)))
+    result.state shouldBe Seq(Some(2),Some(4))
+    result.moves shouldBe Seq(Move(0,0,false),Move(1,1,false))
+    result.doubles shouldBe Seq.empty
   }
   it should "swipe _,2" in {
-    val result = swipe0(Array(None, Some(2), None))
-    assert(result.state === Array(Some(2),None,None))
-    assert(result.moves === Array(Move(1,0,false)))
-    assert(result.doubles === Array())
+    val result = swipe0(Seq(None, Some(2), None))
+    result.state shouldBe Seq(Some(2),None,None)
+    result.moves shouldBe Seq(Move(1,0,false))
+    result.doubles shouldBe Seq.empty
   }
   it should "swipe 2,_,2" in {
-    val result = swipe0(Array(Some(2),None,Some(2)))
-    assert(result.state === Array(Some(4),None,None))
-    assert(result.moves === Array(Move(0,0,false),Move(2,0,true)))
-    assert(result.doubles === Array(0))
+    val result = swipe0(Seq(Some(2),None,Some(2)))
+    result.state shouldBe Seq(Some(4),None,None)
+    result.moves shouldBe Seq(Move(0,0,false),Move(2,0,true))
+    result.doubles shouldBe Seq(0)
   }
   it should "swipe _,2,4" in {
-    val result = swipe0(Array(None,Some(2),Some(4)))
-    assert(result.state === Array(Some(2),Some(4),None))
-    assert(result.moves === Array(Move(1,0,false),Move(2,1,false)))
-    assert(result.doubles === Array())
+    val result = swipe0(Seq(None,Some(2),Some(4)))
+    result.state shouldBe Seq(Some(2),Some(4),None)
+    result.moves shouldBe Seq(Move(1,0,false),Move(2,1,false))
+    result.doubles shouldBe Seq.empty
   }
   it should "swipe _,2,2" in {
-    val result = swipe0(Array(None,Some(2),Some(2)))
-    assert(result.state === Array(Some(4),None,None))
-    assert(result.moves === Array(Move(1,0,false),Move(2,0,true)))
-    assert(result.doubles === Array(0))
+    val result = swipe0(Seq(None,Some(2),Some(2)))
+    result.state shouldBe Seq(Some(4),None,None)
+    result.moves shouldBe Seq(Move(1,0,false),Move(2,0,true))
+    result.doubles shouldBe Seq(0)
   }
   it should "swipe 2,2,2" in {
-    val result = swipe0(Array(Some(2),Some(2),Some(2)))
-    assert(result.state === Array(Some(4),Some(2),None))
-    assert(result.moves === Array(Move(0,0,false),Move(1,0,true),Move(2,1,false)))
-    assert(result.doubles === Array(0))
+    val result = swipe0(Seq(Some(2),Some(2),Some(2)))
+    result.state shouldBe Seq(Some(4),Some(2),None)
+    result.moves shouldBe Seq(Move(0,0,false),Move(1,0,true),Move(2,1,false))
+    result.doubles shouldBe Seq(0)
   }
   it should "swipe 2,2,2,2" in {
-    val result = swipe0(Array(Some(2),Some(2),Some(2),Some(2)))
-    assert(result.state === Array(Some(4),Some(4),None,None))
-    assert(result.moves === Array(Move(0,0,false),Move(1,0,true),Move(2,1,false),Move(3,1,true)))
-    assert(result.doubles === Array(0,1))
+    val result = swipe0(Seq(Some(2),Some(2),Some(2),Some(2)))
+    result.state shouldBe Seq(Some(4),Some(4),None,None)
+    result.moves shouldBe Seq(Move(0,0,false),Move(1,0,true),Move(2,1,false),Move(3,1,true))
+    result.doubles shouldBe Seq(0,1)
   }
   it should "swipe 4,2,2" in {
-    val result = swipe0(Array(Some(4),Some(2),Some(2)))
-    assert(result.state === Array(Some(4),Some(4),None))
-    assert(result.moves === Array(Move(0,0,false),Move(1,1,false),Move(2,1,true)))
-    assert(result.doubles === Array(1))
+    val result = swipe0(Seq(Some(4),Some(2),Some(2)))
+    result.state shouldBe Seq(Some(4),Some(4),None)
+    result.moves shouldBe Seq(Move(0,0,false),Move(1,1,false),Move(2,1,true))
+    result.doubles shouldBe Seq(1)
   }
   it should "swipe 2,4,_,2,2" in {
-    val result = swipe0(Array(Some(2),Some(4),None,Some(2),Some(2)))
-    assert(result.state === Array(Some(2),Some(4),Some(4),None,None))
-    assert(result.moves === Array(Move(0,0,false),Move(1,1,false),Move(3,2,false),Move(4,2,true)))
-    assert(result.doubles === Array(2))
+    val result = swipe0(Seq(Some(2),Some(4),None,Some(2),Some(2)))
+    result.state shouldBe Seq(Some(2),Some(4),Some(4),None,None)
+    result.moves shouldBe Seq(Move(0,0,false),Move(1,1,false),Move(3,2,false),Move(4,2,true))
+    result.doubles shouldBe Seq(2)
   }
   it should "swipe downward" in {
-    val init = Array(None,Some(2),Some(2),Some(2),None)
+    val init = Seq(None,Some(2),Some(2),Some(2),None)
     val result = swipe(init, upward = false)
-    assert(result.state === Array(Some(4),Some(2),None,None,None))
-    assert(result.moves === Array(Move(1,0,false),Move(2,0,true),Move(3,1,false)))
-    assert(result.doubles === Array(0))
+    result.state shouldBe Seq(Some(4),Some(2),None,None,None)
+    result.moves shouldBe Seq(Move(1,0,false),Move(2,0,true),Move(3,1,false))
+    result.doubles shouldBe Seq(0)
   }
   it should "swipe upward" in {
-    val init = Array(None, Some(2), Some(2), Some(2), None)
+    val init = Seq(None, Some(2), Some(2), Some(2), None)
     val result = swipe(init, upward = true)
-    assert(result.state === Array(None,None,None,Some(2),Some(4)))
-    assert(result.moves === Array(Move(3,4,false),Move(2,4,true),Move(1,3,false)))
-    assert(result.doubles === Array(4))
+    result.state shouldBe Seq(None,None,None,Some(2),Some(4))
+    result.moves shouldBe Seq(Move(3,4,false),Move(2,4,true),Move(1,3,false))
+    result.doubles shouldBe Seq(4)
   }
 }
--- src/test/scala/ModelSpec.scala
+++ src/test/scala/ModelSpec.scala
@@ -4,38 +4,38 @@ import org.scalatest.{FlatSpec, Matchers}
 class ModelSpec extends FlatSpec with Matchers {
   it should "swipe left" in {
     val result = swipe(
-      Matrix(3,true,Array(None,None,None,None,Some(2),None,None,None,None)),
+      Matrix(3,true,Seq(None,None,None,None,Some(2),None,None,None,None)),
       Left
     )
-    result.state shouldBe Matrix(3,true,Array(None,None,None,Some(2),None,None,None,None,None))
-    result.moves shouldBe Array(Move((1,1),(0,1),false))
-    result.doubles shouldBe Array()
+    result.state shouldBe Matrix(3,true,Seq(None,None,None,Some(2),None,None,None,None,None))
+    result.moves shouldBe Seq(Move((1,1),(0,1),false))
+    result.doubles shouldBe Seq.empty
   }
   it should "swipe right" in {
     val result = swipe(
-      Matrix(3,true,Array(None,None,None,None,Some(2),None,None,None,None)),
+      Matrix(3,true,Seq(None,None,None,None,Some(2),None,None,None,None)),
       Right
     )
-    result.state shouldBe Matrix(3,true,Array(None,None,None,None,None,Some(2),None,None,None))
-    result.moves shouldBe Array(Move((1,1),(2,1),false))
-    result.doubles shouldBe Array()
+    result.state shouldBe Matrix(3,true,Seq(None,None,None,None,None,Some(2),None,None,None))
+    result.moves shouldBe Seq(Move((1,1),(2,1),false))
+    result.doubles shouldBe Seq.empty
   }
   it should "swipe up" in {
     val result = swipe(
-      Matrix(3,true,Array(None,None,None,None,Some(2),None,None,None,None)),
+      Matrix(3,true,Seq(None,None,None,None,Some(2),None,None,None,None)),
       Up
     )
-    result.state shouldBe Matrix(3,false,Array(None,None,None,Some(2),None,None,None,None,None))
-    result.moves shouldBe Array(Move((1,1),(1,0),false))
-    result.doubles shouldBe Array()
+    result.state shouldBe Matrix(3,false,Seq(None,None,None,Some(2),None,None,None,None,None))
+    result.moves shouldBe Seq(Move((1,1),(1,0),false))
+    result.doubles shouldBe Seq.empty
   }
   it should "swipe down" in {
     val result = swipe(
-      Matrix(3,true,Array(None,None,None,None,Some(2),None,None,None,None)),
+      Matrix(3,true,Seq(None,None,None,None,Some(2),None,None,None,None)),
       Down
     )
-    result.state shouldBe Matrix(3,false,Array(None,None,None,None,None,Some(2),None,None,None))
-    result.moves shouldBe Array(Move((1,1),(1,2),false))
-    result.doubles shouldBe Array()
+    result.state shouldBe Matrix(3,false,Seq(None,None,None,None,None,Some(2),None,None,None))
+    result.moves shouldBe Seq(Move((1,1),(1,2),false))
+    result.doubles shouldBe Seq.empty
   }
 }
